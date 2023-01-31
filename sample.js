50a14bef5922493a83ff29c81d68692cffa0b2fb
kiru98480 <123980025+kiru98480@users.noreply.github.com>
2023-01-30 22:22:11 -0600

    Create sample.js

---
+++ sample.js
@@ -0,0 +1,22 @@
+import 'dotenv/config';
+import express from "express";
+import fs from 'fs';
+import cloneAndPullRepo from './cloneAndPullRepo.js';
+
+const app = express();
+
+app.get('/repos/clone/:username/:repo', (req, res) => {
+  const username = req?.params?.username;
+  const repo = req?.params?.repo;
+  const repoPath = `${username}/${repo}`;
+  const repoExists = fs.existsSync(`repos/${repoPath}`);
+  const confirmation = repoExists ? `Pulling ${repoPath}...` : `Cloning ${repoPath}...`;
+
+  cloneAndPullRepo(repoExists, username, repo, req?.query?.branch);
+  
+  res.status(200).send(confirmation);
+});
+
+app.listen(3000, () => {
+  console.log('App running at http://localhost:3000');
+});
